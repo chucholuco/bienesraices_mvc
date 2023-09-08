1b7ecdfff702b5f64c44c960829c81b65043d1ce
Jesus Tapia <chucholuco@gmail.com>
2023-09-07 22:46:56 -0600

    add functionality for edit a property

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -147,11 +147,81 @@ const almacenarImagen = async (req, res, next) => {
 
 }
 
+const editar = async (req, res) => {
+
+  const { id } = req.params
+
+  // Validar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id)
+  if(!propiedad) {
+    return res.redirect('/mis-propiedades')
+  }
+
+  // Revisar que quien visita la URL, es del usuario
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect('/mis-propiedades')
+  }
+
+  // Consultar modelo de precio y categorias
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll(),
+  ])
+
+  res.render("propiedades/editar", {
+    pagina: `Editar Propiedad ${propiedad.titulo}`,
+    csrfToken: req.csrfToken(),
+    categorias,
+    precios,
+    datos: propiedad,
+  })
+}
+
+const guardarCambios = async (req, res) => {
+
+  // Verificar la validacion
+  // Validacion
+  let resultado = validationResult(req);
+  if (!resultado.isEmpty()) {
+    // Consultar modelo de precio y categorias
+    const [categorias, precios] = await Promise.all([
+      Categoria.findAll(),
+      Precio.findAll(),
+    ]);
+
+    return res.render("propiedades/editar", {
+      pagina: "Editar Propiedad",
+      csrfToken: req.csrfToken(),
+      categorias,
+      precios,
+      errores: resultado.array(),
+      datos: req.body
+    })
+  }
+
+  const { id } = req.params
+
+  // Validar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id)
+  if(!propiedad) {
+    return res.redirect('/mis-propiedades')
+  }
+
+  // Revisar que quien visita la URL, es del usuario
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect('/mis-propiedades')
+  }
+
+  // Reescribir el objeto y actualizarlo
+}
+
 
 export {
     admin,
     crear,
     guardar,
     agregarImagen,
-    almacenarImagen
+    almacenarImagen,
+    editar,
+    guardarCambios
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { admin, crear, guardar, agregarImagen, almacenarImagen } from "../controllers/propiedadController.js";
+import { admin, crear, guardar, agregarImagen, almacenarImagen, editar, guardarCambios } from "../controllers/propiedadController.js";
 import { body } from "express-validator";
 import protegerRruta from '../middleware/protegerRuta.js'
 import upload from '../middleware/subirImagen.js'
@@ -30,5 +30,21 @@ router.post('/propiedades/agregar-imagen/:id',
   upload.single('imagen'),
   almacenarImagen
 )
+
+router.get('/propiedades/editar/:id', protegerRruta, editar)
+
+router.post("/propiedades/editar/:id", protegerRruta,
+  body("titulo").notEmpty().withMessage("El titulo del anuncio es obligatorio"),
+  body("descripcion")
+    .notEmpty().withMessage("La descripcion no puede ir vacia")
+    .isLength({max:200}).withMessage('La Descripcion es muy larga'),
+  body('categoria').isNumeric().withMessage('Selecciona una categoria'),
+  body('precio').isNumeric().withMessage('Selecciona un rango de precios'),
+  body('habitaciones').isNumeric().withMessage('Selecciona la cantidad de habitaciones'),
+  body('estacionamiento').isNumeric().withMessage('Selecciona la cantidad de estacionamientos'),
+  body('wc').isNumeric().withMessage('Selecciona la cantidad de baños'),
+  body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa'),
+  guardarCambios
+);
 
 export default router;
