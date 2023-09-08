d95178b313f487fd48db8eb685779e6331072ef7
Jesus Tapia <chucholuco@gmail.com>
2023-09-08 12:24:57 -0600

    implementing pagination

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -4,24 +4,54 @@ import { Precio, Categoria, Propiedad } from '../models/index.js'
 
 
 const admin = async (req, res) => {
-  const { id } = req.usuario;
 
-  const propiedades = await Propiedad.findAll({
-    where: {
-      usuarioId: id,
-    },
-    include: [
-      {model: Categoria, as: 'categoria'},
-      {model: Precio, as: 'precio'}
-    ]
-  });
+  // Leer QueryString
+  const { pagina: paginaActual } = req.query
 
-  res.render("propiedades/admin", {
-    pagina: "Mis propiedades",
-    propiedades,
-    csrfToken: req.csrfToken()
-  });
-};
+  const regex = /^[0-9]$/
+  if(!regex.test(paginaActual)) {
+    return res.redirect('/mis-propiedades?pagina=1')
+  }
+
+  try {
+    const { id } = req.usuario
+
+    // Limites y Offset para el paginador
+    const limit = 5
+    const offset = (paginaActual * limit) - limit
+
+    const [propiedades, total] = await Promise.all([
+      Propiedad.findAll({
+        limit,
+        offset,
+        where: {
+          usuarioId: id,
+        },
+        include: [
+          {model: Categoria, as: 'categoria'},
+          {model: Precio, as: 'precio'}
+        ]
+      }),
+      Propiedad.count({
+        where: { usuarioId: id}
+      })
+    ])
+  
+    res.render("propiedades/admin", {
+      pagina: "Mis propiedades",
+      propiedades,
+      csrfToken: req.csrfToken(),
+      paginas: Math.ceil(total / limit),
+      paginaActual: Number(paginaActual),
+      total,
+      offset,
+      limit
+    })
+  } catch (error) {
+    console.log(error)
+  }
+ 
+}
 
 const crear = async (req, res) => {
     // Consultar modelo de precio y categorias
