f5bfb7f7b8bd8713fa0ed981ad0a1acbea02f75f
Jesus Tapia <chucholuco@gmail.com>
2023-09-08 18:09:35 -0600

    adding feature for the search field

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,3 +1,4 @@
+import { Sequelize } from 'sequelize'
 import { Precio, Categoria, Propiedad } from '../models/index.js'
 
 const inicio = async (req, res) => {
@@ -36,7 +37,8 @@ const inicio = async (req, res) => {
        categorias,
        precios,
        casas,
-       departamentos 
+       departamentos,
+       csrfToken: req.csrfToken()
     })
 }
 
@@ -61,18 +63,44 @@ const categoria = async (req, res) => {
 
     res.render('categoria', {
         pagina: `${categoria.nombre}s en venta`,
-        propiedades
+        propiedades,
+        csrfToken: req.csrfToken()
     })
 
 }
 
 const noEncontrado = (req, res) => {
-
+    res.render('404', {
+        pagina: 'No Encontrada',
+        csrfToken: req.csrfToken()
+    })
 }
 
-const buscador = (req, res) => {
+const buscador = async (req, res) => {
+  const { termino } = req.body;
 
+  // Validar que termino no este vacio
+  if (!termino.trim()) {
+    return res.redirect("back");
+  }
+
+  // Consultar las propiedades
+  const propiedades = await Propiedad.findAll({
+    where: {
+        titulo: {
+            [Sequelize.Op.like] : '%' + termino + '%'
+        }
+    },
+    include: [{ model: Precio, as: "precio" }],
+  })
+  
+  res.render('busqueda', {
+    pagina: 'Resultados de la Busqueda', 
+    propiedades,
+    csrfToken: req.csrfToken()
+  })
 }
+
 
 export {
     inicio,
